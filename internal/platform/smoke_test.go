5eaef63b017dac7a14e727a681eb1258271f90d6
YuDong999 <308492886@qq.com>
2026-08-29 14:10:25 +0800

    test(platform): 冒烟矩阵补充 Rocky Linux 9 (RHEL克隆对称覆盖)
    - 与 AlmaLinux 9 对称,同为 redhat/dnf/systemd/lsblk-J,覆盖 RHEL 下游双分支
    - 二进制体积 51M 未膨胀(加入 platform 框架前后一致)

--- internal/platform/smoke_test.go
+++ internal/platform/smoke_test.go
@@ -46,6 +46,13 @@ func TestDistroSmokeMatrix(t *testing.T) {
 			fwBackend: "firewalld", svcManager: "systemd",
 		},
 		{
+			name: "Rocky Linux 9",
+			probe: probeBlock("rocky", "9", "rhel fedora", "x86_64", "systemd",
+				"1", "1", "1", "1", "0", "1", "1", "1", "1", "1", "1"),
+			fam: FamilyRedHat, pm: PkgDNF, init: InitSystemd, lsblkJSON: true,
+			fwBackend: "firewalld", svcManager: "systemd",
+		},
+		{
 			name: "银河麒麟 V10",
 			probe: probeBlock("kylin", "V10", "", "aarch64", "systemd",
 				"1", "0", "1", "1", "0", "1", "0", "1", "1", "1", "0"),
